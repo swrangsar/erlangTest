55b301adc8b88dbb1ad92c31539439ebf656c023
swrangsar basumatary <swrangsar@example.com>
2014-07-05 15:42:12 +0530

    commit at 20140705154212

--- remApp/src/event.erl
+++ remApp/src/event.erl
@@ -5,10 +5,25 @@
                 to_go=0}).
 
 
-loop(S = #state{server=Server}) ->
+%% Because Erlang is limited to about 49 days (49*24*60*60*1000) in
+%% milliseconds, the following function is used
+normalize(N) ->
+    Limit = 49*24*60*60,
+    [N rem Limit | lists:duplicate(N div Limit, Limit)].
+
+
+%% Loop uses a list for times in order to go around the ~49 days limit
+%% on timeouts.
+loop(S = #state{server=Server, to_go=[T|Next]}) ->
     receive
         {Server, Ref, cancel} ->
             Server ! {Ref, ok}
-    after S#state.to_go*1000 ->
-        Server ! {done, S#state.name}
+    after T*1000 ->
+        if Next =:= [] ->
+            Server ! {done, S#state.name};
+           Next =/= [] ->
+            loop(S#state{to_go=Next})
+        end
     end.
+
+
